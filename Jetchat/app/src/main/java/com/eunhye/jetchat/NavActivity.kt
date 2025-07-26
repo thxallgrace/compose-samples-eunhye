0e447f0cbb18c4e346592f02f6493274a1f447ae
thxallgrace <thxall92@github.com>
2025-07-26 18:58:31 +0900

    AppCompatActivity에서 확장함수로 setContent 선언해 Compose UI 표시

--- Jetchat/app/src/main/java/com/eunhye/jetchat/NavActivity.kt
+++ Jetchat/app/src/main/java/com/eunhye/jetchat/NavActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.core.view.ViewCompat
 import androidx.navigation.NavController
@@ -22,15 +20,16 @@ class NavActivity : AppCompatActivity() {
         // 윈도우 인셋 : 상태바, 네비게이션바, IME 등 시스템 UI 컴포넌트로 인해 앱이 가려지는 영역 정보를 담고 있음
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, insets -> insets }
 
-        // setContent {}는 Compose에서 제공하는 ComponentActivity를 기반으로 한 확장 함수이므로
-        // ComposeView를 직접 생성해 setContent {}를 정의할 수 있다
-        setContentView(
-            ComposeView(this).apply {
-                // consumeWindowInsets : ComposeView 에 전달되는 윈도우 인셋 (기본값은 true)
-                // false로 설정할 경우, ComposeView가 윈도우 인셋 이벤트를 소비하지 않고, 그대로 부모 뷰나 다른 곳에 전달
-                consumeWindowInsets = false
-            }
-        )
+        // ComponentActivity 확장함수(setContent)로 내부적으로 컴포즈뷰를 만들어 주기 때문에 커스터마이징이 필요할때만 setContentView를 통해 컴포즈뷰 직접 생성
+        // 커스터마이징의 예는 consumeWindowInsets = false와 같은걸 얘기함.
+//        setContentView(
+//            ComposeView(this).apply {
+//                // consumeWindowInsets : ComposeView 에 전달되는 윈도우 인셋 (기본값은 true)
+//                // false로 설정할 경우, ComposeView가 윈도우 인셋 이벤트를 소비하지 않고, 그대로 부모 뷰나 다른 곳에 전달
+//                consumeWindowInsets = false
+//            }
+//        )
+
         setContent {
             JetchatDrawer() {
                 // 기존 View기반 레이아웃(XML, ViewBinding)화면을 Compose영역에 삽입하는 것
